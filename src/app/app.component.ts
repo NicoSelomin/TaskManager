54bbf6746a17cc1667e310430cf9604f1f16b37d
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-02-21 19:07:14 +0100

    home component created for the home page design delocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TaskManager';
+export class AppComponent implements OnInit {
+  isDarkMode = false;
+
+  ngOnInit() {
+    // Vérifier si un mode est stocké dans localStorage
+    const storedTheme = localStorage.getItem('theme');
+    this.isDarkMode = storedTheme === 'dark';
+
+    // Appliquer le mode sombre si nécessaire
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
