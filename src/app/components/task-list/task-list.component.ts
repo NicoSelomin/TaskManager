eae1c237c8bce797d35e0ce19076635f26935818
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-02-22 18:12:33 +0100

    task-list changed : state, and buttons add and new methods to change state, and delete

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../../models/task.model';
 import { TaskServicesService } from '../../services/task-services.service'
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
@@ -12,7 +13,7 @@ import { CommonModule } from '@angular/common';
 })
 export class TaskListComponent implements OnInit  {
   tasks : Task[] = [];
-  constructor(private taskService: TaskServicesService) { }
+  constructor(private taskService: TaskServicesService, private router: Router) { }
   ngOnInit(): void {
     this.loadTasks();
   }
@@ -26,6 +27,22 @@ export class TaskListComponent implements OnInit  {
     this.loadTasks();
   }
 
-  
+  markTaskAsCompleted(task: Task) {
+    task.isCompleted = true;
+    this.taskService.updateTask(task);
+    this.loadTasks(); // Rafraîchir la liste après modification
+  }
+
+  editTask(task: Task) {
+    // Redirection vers une page d'édition (si tu as une page dédiée à l'édition)
+    this.router.navigate(['/edit-task', task.idTask]);
+  }
+
+  deleteTask(idTask: number) {
+    this.taskService.deleteTask(idTask);
+    this.loadTasks(); // Rafraîchir la liste après suppression
+  }
+
+
 
 }
